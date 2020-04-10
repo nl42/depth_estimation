7264e272fe3ffe2e6219f886dec1e2d744d6761c
Nick Lindfield <nick.lindfield@btinternet.com>
2020-04-10 17:07:47 +0100

    Completed local absolute depth

--- depth_Functions.py
+++ depth_Functions.py
@@ -12,14 +12,15 @@ feature_descriptors =   {
 
 def show_img(im, ax=None, figsize=(8,8)):
     if not ax: _,ax = plt.subplots(1,1,figsize=figsize)
-    if len(im.shape)==2: im = np.tile(im[:,:,None], 3)
-    ax.imshow(im[:,:,::-1])
+    # if len(im.shape)==2: im = np.tile(im[:,:,None], 3)
+    if len(im.shape)==3: im = im[:,:,::-1]
+    ax.imshow(im)
     ax.xaxis.set_visible(False)
     ax.yaxis.set_visible(False)
     return ax
 
 
-def array_of_images(images, width, height, figsize=(16,16)):
+def show_list_of_images(images, height, width, figsize=(16,16)):
     _,ax = plt.subplots(height, width, figsize=figsize)
 
     for index, image in enumerate(images):
@@ -27,6 +28,12 @@ def array_of_images(images, width, height, figsize=(16,16)):
             image = cv2.normalize(image, None, 0, 1, cv2.NORM_MINMAX)
         show_img(image, ax[int(index/width)][index%width])
 
+def show_array_of_images(images, figsize=(16,16)):
+    _,ax = plt.subplots(images.shape[0], images.shape[1], figsize=figsize)
+    
+    for row_number, row in enumerate(array):
+        for column_number, image in enumerate(row):
+            show_img(image, ax=ax[row_number][column_number])
 
 def import_raw_colour_image(path):
     with open(path, 'rb') as f:
@@ -40,11 +47,6 @@ def import_raw_depth_image(path):
         
     image = np.frombuffer(buffer, dtype="float32").reshape(int(720), -1)
     return image
-
-def show_depth_image(image, size=(18,32)):
-    plt.figure(figsize = size) ## make the figure larger or smaller, keep the aspect ratio (9:16) the same
-    plt.imshow(image, interpolation='nearest')
-    plt.axis("off")
 
 def plot_image_descriptors(image, key_points):
     image_copy = image.copy()
